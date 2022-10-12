d10312fcba15fa31b8a25cbd1e6cfb769337c044
dilawer18 <dilawer1807948@gmail.com>
2022-10-12 18:54:05 +0530

    change in state

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,76 +1,47 @@
-// import React, { Component } from 'react';
 
-// import { Text, View, Button, TouchableOpacity ,StyleSheet} from 'react-native';
-// import style from './Style';
-
-// class Home extends Component {
-
-//   render() {
-//     function fun() {
-//       alert("React Native")
-//     }
-
-//     return (
-//       <View style={style.TOpView}>
-
-//       <View style={style.container}>
-//         <Text style={style.fonts}>My App</Text>
-//         <View style={style.container}>
-//         <TouchableOpacity
-//           onPress={fun}
-//           activeOpacity={0.7}
-//           style={style.touchBtn}>
-//           <Text style={{ color: 'white' }}>Continue</Text>
-//         </TouchableOpacity>
-//         </View>
-
-//       </View>
-//       </View>
-//     );
-
-//   }
-
-// }
-
-// export default Home;
-
-//import liraries
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity,Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styles from './Style'
 
 // create a component
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'My App'
+    }
+  }
   render() {
-    function fun() {
-            alert("React Native")
-          }
+    // function fun() {
+    //          alert("React Native")
+    //         this.setState({name:'hello'})
+    //       }
     return (
       <View style={styles.container}>
 
         <View style={styles.imagestyle}>
-          <Image style={styles.ImgStyle} source={require('../../assets/Images/images.png')}/>
-          <Image style={styles.logostyle} source={require('../../assets/Images/logo.png')}/>
-          <Image style={styles.ImgStyle} source={require('../../assets/Images/edit.png')}/>
+          <Image style={styles.ImgStyle} source={require('../../assets/Images/images.png')} />
+          <Image style={styles.logostyle} source={require('../../assets/Images/logo.png')} />
+          <Image style={styles.ImgStyle} source={require('../../assets/Images/edit.png')} />
         </View>
         <View style={styles.ViewText}>
           <Text style={styles.txtStyle}>
-            My App
+            {this.state.name}
           </Text>
 
         </View>
-    <View>
-    <TouchableOpacity
-        style={styles.btn}
-          onPress={fun}
-          activeOpacity={0.7}>
-          <Text style={{ color: 'white' }}>Click Me</Text>
-        </TouchableOpacity>
-      
-    </View>
-        
-        
-              </View>
+        <View>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={() => this.setState({ name: 'React Native' })}
+            activeOpacity={0.7}>
+            <Text style={{ color: 'white' }}>Click Me</Text>
+          </TouchableOpacity>
+
+        </View>
+
+
+      </View>
     );
   }
 }
